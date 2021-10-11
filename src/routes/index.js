b624a948754d23205b0265e04a00eb738bb2b936
gamez45 <81650381+gamez45@users.noreply.github.com>
2021-10-11 09:16:42 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,4 +8,16 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
+router.get('/productlist', (req, res) => {
+  res.render('productlist');
+});
+
+router.get('/addproduct', (req, res) => {
+  res.render('addproduct');
+});
+
+router.get('/mercadolibre', (req, res) => {
+  res.render('mercadolibre');
+});
+
 module.exports = router;
